f8bbd43a3903e8188e4fb7542c8af71b6a1f4fde
shevcari000 <shevcari000@CAM-LAB-22.bgsd.k12.wa.us>
2019-10-25 07:51:54 -0700

    not done

--- src/Equestria.java
+++ src/Equestria.java
@@ -2,8 +2,14 @@
 public class Equestria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double x1 = 31;
+	    double y1 = 15;
+	    double x2 = 35;
+	    double y2 = 9;
 
 	}
-
+	public static double distance(double x1, double y1, double x2, double y2) {
+		double Step1 = x2 - x1;
+		double Step2 = y2 - y1;
+		double Distance = Step1(Math.pow(base, exponent)) + Step2(Math.pow(base, exponent));
 }
